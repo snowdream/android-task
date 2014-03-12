e3ea3950e0fdc6b7cb79c424e1dd5a3539476829
snowdream <yanghui1986527@gmail.com>
2014-03-12 09:59:59 +0800

    Added:  Add Executor THREAD_POOL_EXECUTOR  and  Executor SERIAL_EXECUTOR  from android.os.AsyncTask

--- lib/src/main/java/com/github/snowdream/android/core/Task.java
+++ lib/src/main/java/com/github/snowdream/android/core/Task.java
@@ -16,15 +16,86 @@
 
 package com.github.snowdream.android.core;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import com.github.snowdream.android.util.Log;
 
+import java.util.ArrayDeque;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by snowdream on 3/11/14.
  */
 public class Task<Input, Progress, Output> {
+    private static final String LOG_TAG = "Task";
+
+    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+    private static final int CORE_POOL_SIZE = CPU_COUNT + 1;
+    private static final int MAXIMUM_POOL_SIZE = CPU_COUNT * 2 + 1;
+    private static final int KEEP_ALIVE = 1;
+
+    private static final ThreadFactory sThreadFactory = new ThreadFactory() {
+        private final AtomicInteger mCount = new AtomicInteger(1);
+
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "Task #" + mCount.getAndIncrement());
+        }
+    };
+
+    private static final BlockingQueue<Runnable> sPoolWorkQueue =
+            new LinkedBlockingQueue<Runnable>(128);
+
+    /**
+     * An {@link Executor} that can be used to execute tasks in parallel.
+     */
+    public static final Executor THREAD_POOL_EXECUTOR
+            = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE,
+            TimeUnit.SECONDS, sPoolWorkQueue, sThreadFactory);
+
+    /**
+     * An {@link Executor} that executes tasks one at a time in serial
+     * order.  This serialization is global to a particular process.
+     */
+    public static final Executor SERIAL_EXECUTOR = new SerialExecutor();
+
+    private static volatile Executor sDefaultExecutor = SERIAL_EXECUTOR;
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    private static class SerialExecutor implements Executor {
+        final ArrayDeque<Runnable> mTasks = new ArrayDeque<Runnable>();
+        Runnable mActive;
+
+        public synchronized void execute(final Runnable r) {
+            mTasks.offer(new Runnable() {
+                public void run() {
+                    try {
+                        r.run();
+                    } finally {
+                        scheduleNext();
+                    }
+                }
+            });
+            if (mActive == null) {
+                scheduleNext();
+            }
+        }
+
+        protected synchronized void scheduleNext() {
+            if ((mActive = mTasks.poll()) != null) {
+                THREAD_POOL_EXECUTOR.execute(mActive);
+            }
+        }
+    }
+
+    /**
+     * Default Executor
+     * @param exec
+     */
+    public static void setDefaultExecutor(Executor exec) {
+        sDefaultExecutor = exec;
+    }
 
     private final boolean enableLog;
     private final List<Task> tasks;
